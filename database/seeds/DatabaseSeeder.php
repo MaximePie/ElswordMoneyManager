39c8133abf2c1115c562c59945e0f0e14d031bb7
Chatmalow <maxime.pie.mail@gmail.com>
2020-09-11 08:46:32 +0200

    Feat(back): Add Dungeon model

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Character;
+use App\Product;
+use App\Productprice;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+    	/** @var Character $character */
+    	$character = Character::Create(['name' => 'Laby']);
+    	$product = Product::Create(['name' => 'Fragment d\'Eldrit', 'character_id' => $character->id]);
+
+    	/** @var Productprice $productPrice */
+    	$productPrice = Productprice::Create(['price' => 499, 'product_id' => $product->id]);
+
+    	/** @var Productprice $secondProductPrice */
+    	$secondProductPrice = Productprice::Create(['price' => 799, 'last_product_price_id' => $productPrice->id, 'product_id' => $product->id]);
+    	$productPrice->next_product_price_id = $secondProductPrice->id;
+    	$productPrice->save();
     }
 }
